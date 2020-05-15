07246fc2ed8a9f2851e9b9f05ac6e386e1f895a2
Aditi <gargaditi08>
2020-05-15 14:41:49 +0530

    Initial Commit

---
+++ LlyodsBankAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LlyodsBankAssignment
+//
+//  Created by Aditi Garg on 15/05/20.
+//  Copyright © 2020 Aditi Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
